7ab1930c21af5f08e41bddb94df0971418c6b9de
Blake Davis <blavis21@gmail.com>
2019-06-27 13:43:40 -0500

    finished daily journal 7

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,10 +1,12 @@
 //function that converts data into html -- to be inserted dynamically
-function makeJournalEntryComponent (input) {
+function makeJournalEntryComponent(input) {
     return `
     <div class="entry">
-        <h2>Date: ${input.date}</h2>
-        <h3>Mood: ${input.mood}</h3>
-        <h3>Concept: ${input.concept}</h3>
-        <h3>Entry: ${input.entry}</h3>
+        <section class="newEntry">
+            <h2>Date: ${input.date}</h2>
+            <h3>Mood: ${input.mood}</h3>
+            <h3>Concept: ${input.concept}</h3>
+            <h3>Entry: ${input.entry}</h3>
+        </section>
     </div>`
 };
